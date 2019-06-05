211081ab7f4d89c7dd31e19e080f71ef9ae3376a
tommaso auerbach <tsa302@nyu.edu>
2019-06-05 21:23:27 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,12 +3,12 @@ require 'pry'
 class String
 
   def sentence?
-if self.end_with?(".")
-  return true
-else
-  return false
-end
-end
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
+  end
 
   def question?
     if self.end_with?("?")
@@ -27,6 +27,7 @@ end
   end
 
   def count_sentences
-
+    new_array = self.split(/[.?!]/).reject {|string| string.empty?}
+    return new_array.length
   end
 end
